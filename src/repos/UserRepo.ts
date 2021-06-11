f0bb2e85b06a108a7575663fd9b48967c615ea23
IvanLisas <ivanelisas@gmail.com>
2021-06-11 20:25:24 -0300

    Correccion

--- src/repos/UserRepo.ts
+++ src/repos/UserRepo.ts
@@ -2,6 +2,8 @@ import { EntityRepository, Repository } from 'typeorm'
 import { User } from '../models/User'
 
 @EntityRepository(User)
-export class UserRepo extends Repository<User> {
+class UserRepo extends Repository<User> {}
 
-}
+const userRepo = new UserRepo()
+
+export default userRepo
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,21 +1,13 @@
 import { getRepository } from 'typeorm'
-import { UserRepo } from '../repos/UserRepo'
+import userRepo from '../repos/UserRepo'
 import { User } from '../models/User'
 
-
 export class UserService {
-    
-    userRepo = getRepository(User)  
-
-    async login(anEmail: string, aPassword: string) : Promise<User>{
-        try {
-            return await this.userRepo.findOneOrFail({ email: anEmail, password:aPassword })
-          } catch (error) {
-            throw 'Credenciales incorrectas'
-          }
+  async login(anEmail: string, aPassword: string): Promise<User> {
+    try {
+      return await userRepo.findOneOrFail({ email: anEmail, password: aPassword })
+    } catch (error) {
+      throw 'Credenciales incorrectas'
     }
-
+  }
 }
-
-
-
