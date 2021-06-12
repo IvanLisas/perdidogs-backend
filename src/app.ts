afffec0f6f448d0a47518c65f7f00e6893808e06
IvanLisas <ivanelisas@gmail.com>
2021-06-12 09:02:57 -0300

    Se hace la conexion con mysql

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,47 @@
-import express from 'express'
-import chatRoutes from './routes/chat.routes'
-import { createConnection } from "typeorm";
+import express, { Request, Response } from 'express'
+import { createConnection } from 'typeorm'
 
-class App {
-  public server
+//ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '1234'
+//Tirar este query del ojete en el sql
+class Server {
+  private app: express.Application
 
   constructor() {
-    this.server = express()
-
-    this.middlewares()
-
+    this.app = express() // init the application
+    this.configuration()
     this.routes()
-
-    createConnection();
   }
 
-  
-  middlewares() {
-    this.server.use(express.json())
+  /**
+   * Method to configure the server,
+   * If we didn't configure the port into the environment
+   * variables it takes the default port 3000
+   */
+  public configuration() {
+    this.app.set('port', process.env.PORT || 3001)
+    this.app.use(express.json())
   }
 
-  routes() {
-    this.server.use([chatRoutes])
+  /**
+   * Method to configure the routes
+   */
+  public async routes() {
+    await createConnection()
+
+    this.app.get('/', (req: Request, res: Response) => {
+      res.send('Hello world!')
+    })
+  }
+
+  /**
+   * Used to start the server
+   */
+  public start() {
+    this.app.listen(this.app.get('port'), () => {
+      console.log(`Server is listening ${this.app.get('port')} port.`)
+    })
   }
 }
 
-export default new App().server
+const server = new Server() // Create server instance
+server.start() // Execute the server
