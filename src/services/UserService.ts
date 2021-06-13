b873129171d2ac18864d59e254337937b476c2c7
IvanLisas <ivanelisas@gmail.com>
2021-06-13 01:21:15 -0300

    Cambio

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,37 +1,26 @@
-import userRepo, { UserRepo } from '../repos/UserRepo'
 import { User } from '../models/User'
-import { getCustomRepository, getRepository } from 'typeorm'
+import { getRepository } from 'typeorm'
 
 class UserService {
   async login(anEmail: string, aPassword: string): Promise<User> {
     try {
-      return await userRepo.findOneOrFail({ email: anEmail, password: aPassword })
+      return await getRepository(User).findOneOrFail({ email: anEmail, password: aPassword })
     } catch (error) {
       throw 'Credenciales incorrectas'
     }
   }
 
-  async getUser(id: number): Promise<User> {
-    try {
-      return await userRepo.findOneOrFail({ userId: id })
-    } catch (error) {
-      throw 'No existe el usuario'
-    }
+  async get(id: number): Promise<User> {
+    return await getRepository(User).findOneOrFail(id)
   }
 
-  async saveUser(user: User): Promise<User> {
-    /*   if (!user.name || !user.surname || !user.email || !user.password || !user.birthdate) throw 'Usuario inválido' */
+  async save(user: User): Promise<User> {
     return await getRepository(User).save(user)
   }
 
-  async deleteUser(user: User): Promise<User> {
-    try {
-      user.isActive = false
-      if (!userRepo.find(user)) throw 'Usuario inválido'
-      else return await userRepo.save(user)
-    } catch (error) {
-      throw 'Credenciales incorrectas'
-    }
+  async delete(user: User): Promise<User> {
+    user.isActive = false
+    return await getRepository(User).save(user)
   }
 }
 
