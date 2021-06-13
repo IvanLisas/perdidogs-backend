ff2480f35182186c2f201694efef9ad73aeda01c
IvanLisas <ivanelisas@gmail.com>
2021-06-12 22:34:19 -0300

    Refactor

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -5,37 +5,23 @@ import postService, { PostService } from '../services/PostService'
 
 const postRoutes = Router()
 
-module.exports = function (app: express.Application) {
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*') // allow requests from any other server
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE') // allow these verbs
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Cache-Control')
-    next()
-  })
+postRoutes.get('/getAllPosts/:PostId', (req, res) => {
+  const post = parseInt(req.params.postId)
+  return res.json(postService.getAllPosts(post))
+})
 
-  postRoutes.get('/getAllPosts/:PostId', (req, res) => {
-    const post = parseInt(req.params.postId)
-    return res.json(postService.getAllPosts(post))
-  })
+postRoutes.post('/createPost', (req, res) => {
+  return res.json(postService.createPost(req.body))
+})
 
-  postRoutes.post('/createPost', (req, res) => {
-    return res.json(postService.createPost(req.body))
-  })
-   
+postRoutes.get('/getAPost/:PostId', (req, res) => {
+  const post = parseInt(req.params.postId)
+  return res.json(postService.getAPostById(req.body))
+})
 
-  postRoutes.get('/getAPost/:PostId', (req, res) => {
-    const post = parseInt(req.params.postId)
-    return res.json(postService.getAPostById(req.body))
-  }) 
-
-  postRoutes.get('/updateAPost/:PostId', (req, res) => {
-    const post = parseInt(req.params.postId)
-    return res.json(postService.getAPostById(req.body))
-  }) 
-}
-}
-
-
-
+postRoutes.get('/updateAPost/:PostId', (req, res) => {
+  const post = parseInt(req.params.postId)
+  return res.json(postService.getAPostById(req.body))
+})
 
 export default postRoutes
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,27 +4,17 @@ import userService from '../services/UserService'
 
 const chatRoutes = Router()
 
-module.exports = function (app: express.Application) {
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*') // allow requests from any other server
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE') // allow these verbs
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Cache-Control')
-    next()
-  })
+chatRoutes.get('/getUser/:userId', (req, res) => {
+  const id = parseInt(req.params.userId)
+  return res.json(userService.getUser(id))
+})
 
-  chatRoutes.get('/getUser/:userId', (req, res) => {
-    const id = parseInt(req.params.userId)
-    return res.json(userService.getUser(id))
-  })
+chatRoutes.post('/saveUser', (req, res) => {
+  return res.json(userService.saveUser(req.body))
+})
 
-  chatRoutes.post('/saveUser', (req, res) => {
-    return res.json(userService.saveUser(req.body))
-  })
-
-  chatRoutes.delete('/deleteUser', (req, res) => {
-    return res.json(userService.saveUser(req.body))
-  })
-
-}
+chatRoutes.delete('/deleteUser', (req, res) => {
+  return res.json(userService.saveUser(req.body))
+})
 
 export default chatRoutes
