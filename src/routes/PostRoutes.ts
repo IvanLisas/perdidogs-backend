81e2f5cb20ec4aaa21386c47a9fda240b6b478d6
Ivan <ivanelisas@gmail.com>
2021-06-24 03:08:05 -0300

    cambios

--- src/routes/PostRoutes.ts
+++ src/routes/PostRoutes.ts
@@ -37,8 +37,9 @@ postRoutes.get('/getAll', async (req, res) => {
 })
 
 //GET BY FILTER post
-postRoutes.get('/by-filter', async (req, res) => {
+postRoutes.put('/by-filter', async (req, res) => {
   try {
+    console.log(req.body)
     return res.json(await postService.getPostByFilters(req.body as Filter))
   } catch (error) {
     res.status(400).send(error.message)
