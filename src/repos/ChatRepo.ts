3e50c901471a64c8b998cf09cee3790cac4e843c
LoyGabriel <gabrielloy01@gmail.com>
2021-07-17 22:46:35 -0300

    ORDEN DE LOS CHATS

--- src/repos/ChatRepo.ts
+++ src/repos/ChatRepo.ts
@@ -3,15 +3,4 @@ import { Chat } from '../models/Chat'
 
 @EntityRepository(Chat)
 export class ChatRepo extends Repository<Chat> {
-    static async getByOwnerId():Promise<Chat[]>{
-        const entityManager = getManager();
-        const counts = await entityManager.query(`select  c.*,m.* from perdidogs.chat c
-        inner join perdidogs.message m
-        on m.chatId= c.id
-        where (c.ownerId=3 OR c.owner2Id=3)
-        order by m.creationDate desc`);
-        return counts
-      }
-
-
 }
