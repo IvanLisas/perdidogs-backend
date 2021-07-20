95d17301a930fb4b0fd9f4df99809002d021d29d
LoyGabriel <gabrielloy01@gmail.com>
2021-07-19 23:06:56 -0300

    Mejora en el query builder de las alertas

--- src/repos/AlertRepo.ts
+++ src/repos/AlertRepo.ts
@@ -1,24 +1,54 @@
 import { EntityRepository, getManager, Repository } from 'typeorm'
-import { QueryResult } from '../admin-module/models/QueryResult';
+import { QueryResult } from '../admin-module/models/QueryResult'
 import { Alert } from '../models/Alert'
-import { Pet } from '../models/Pet';
+import { Pet } from '../models/Pet'
 
 @EntityRepository(Alert)
 export class AlertRepo extends Repository<Alert> {
-    static async filterAlertsByPetInPost(pet:Pet):Promise<QueryResult[]>{
-        console.log("LLEGA AL ALERT REPO", pet)
-        const entityManager = getManager();
-        const counts = await entityManager.query(
-          `SELECT a.Id as alertOrPostId,a.locationId, pet.* FROM Alert a
-          INNER JOIN pet  
-          ON a.petId= pet.Id
-          WHERE pet.colorId= `+ pet.color.Id+
-          ' AND pet.furLengthId= '+ pet.furLength.Id +
-          ' AND pet.breedId= '+ pet.breed.Id +
-          ' AND pet.hasCollar= '+ pet.hasCollar + 
-          ' AND pet.sizeId= ' + pet.size.Id);
-        return counts
-      }
+  static filterAlertsByPetInPostQuery = 'SELECT a.Id as alertOrPostId,a.locationId, pet.* FROM Alert a INNER JOIN pet ON a.petId= pet.Id'
 
+  static async filterAlertsByPetInPost(pet: Pet): Promise<QueryResult[]> {
+    console.log('LLEGA AL ALERT REPO', pet)
+    const entityManager = getManager()
+    const query = AlertRepo.filterAlertsByPetInPostQuery + buildWhereStatements(pet)
+    const counts = await entityManager.query(query)
+    return counts
+  }
 
+ 
 }
+function buildWhereStatements(pet: Pet): string {
+    let query = ''
+    if (pet.furLength != undefined) {
+      if (query.length < 5) {
+        query = query + ' WHERE'
+      }
+      query = query + ' pet.furLengthId= ' + pet.furLength.Id
+    }
+    if (pet.color !== undefined) {
+      if (query.length < 5) {
+        query = query + ' WHERE'
+      }
+      query = query + ' AND pet.colorId= ' + pet.color.Id
+    }
+    if (pet.breed !== undefined) {
+      if (query.length < 5) {
+        query = query + ' WHERE'
+      }
+      query = query + ' AND pet.breedId= ' + pet.breed.Id
+    }
+    if (pet.size !== undefined) {
+      if (query.length < 5) {
+        query = query + ' WHERE'
+      }
+      query = query + ' AND pet.sizeId= ' + pet.size.Id
+    }
+    if (pet.hasCollar !== undefined) {
+      if (query.length < 5) {
+        query = query + ' WHERE'
+      }
+      query = query + ' AND pet.hasCollar=  ' + pet.hasCollar
+    }
+    return query
+  }
+
--- src/repos/PostRepo.ts
+++ src/repos/PostRepo.ts
@@ -1,50 +1,82 @@
 import { EntityRepository, getManager, Repository } from 'typeorm'
-import { Count } from '../admin-module/models/Count';
-import { QueryResult } from '../admin-module/models/QueryResult';
-import { NotificationDTO } from '../models/NotificationDTO';
-import { Pet } from '../models/Pet';
+import { Count } from '../admin-module/models/Count'
+import { QueryResult } from '../admin-module/models/QueryResult'
+import { NotificationDTO } from '../models/NotificationDTO'
+import { Pet } from '../models/Pet'
 import { Post } from '../models/Post'
- 
+
 @EntityRepository(Post)
 export class PostRepo extends Repository<Post> {
-
-    static async countLostBreeds():Promise<Count[]>{
-        const entityManager = getManager();
-        const counts = await entityManager.query(`SELECT COUNT(b.Id) as count, b.Id, b.description from post p
+  static filterPostByPetAlertQuery = 'SELECT p.Id as alertOrPostId,p.locationId, pet.* FROM Post p INNER JOIN pet  ON p.petId= pet.Id'
+  static async countLostBreeds(): Promise<Count[]> {
+    const entityManager = getManager()
+    const counts = await entityManager.query(`SELECT COUNT(b.Id) as count, b.Id, b.description from post p
         INNER JOIN pet 
         on p.petId= pet.id
         INNER JOIN breed b
         on pet.breedId = b.id
-        group by b.Id`);
-        return counts
-      }
+        group by b.Id`)
+    return counts
+  }
 
-      static async filterPostByPetAlert(pet:Pet):Promise<QueryResult[]>{
-        const entityManager = getManager();
-        const counts = await entityManager.query(
-          `SELECT p.Id as alertOrPostId,p.locationId, pet.* FROM Post p 
-          INNER JOIN pet  
-          ON p.petId= pet.Id
-          WHERE pet.furLengthId= `+ pet.furLength.Id +
-          ' AND pet.colorId= '+ pet.color.Id +
-          ' AND pet.breedId= '+ pet.breed +
-          ' AND pet.hasCollar= '+ pet.hasCollar + 
-          ' AND pet.sizeId= ' + pet.size.Id)
-        return counts
-      }
+  static async filterPostByPetAlert(pet: Pet): Promise<QueryResult[]> {
+    const entityManager = getManager()
+    const query = PostRepo.filterPostByPetAlertQuery + buildWhereStatements(pet)
+    console.log(query)
+    const counts = await entityManager.query(query)
+    return counts
+  }
 
-      static async getPostsByUserId(userId:number):Promise<NotificationDTO[]>{
-        console.log("LLEGA AL getAlertsByUserId() ")
-        const entityManager = getManager();
-        return  await entityManager.query('SELECT DISTINCT p.*, n.alertId, n.postId FROM  perdidogs.user u INNER JOIN post p '+
-        ' ON p.ownerId = u.Id '+
-        ' INNER JOIN notifications n '+
-        ' ON n.postId = p.Id '+
-        ' inner join alert a '+
-        ' on a.Id= n.alertId '+
+  static async getPostsByUserId(userId: number): Promise<NotificationDTO[]> {
+    console.log('LLEGA AL getAlertsByUserId() ')
+    const entityManager = getManager()
+    return await entityManager.query(
+      'SELECT DISTINCT p.*, n.alertId, n.postId FROM  perdidogs.user u INNER JOIN post p ' +
+        ' ON p.ownerId = u.Id ' +
+        ' INNER JOIN notifications n ' +
+        ' ON n.postId = p.Id ' +
+        ' inner join alert a ' +
+        ' on a.Id= n.alertId ' +
         ' AND n.hasBeenRejected= false' +
-        ' AND p.ownerId!= '+userId +
-        ' AND a.ownerId= '+userId);
-      }
+        ' AND p.ownerId!= ' +
+        userId +
+        ' AND a.ownerId= ' +
+        userId
+    )
+  }
 }
 
+function buildWhereStatements(pet: Pet): string {
+  let query = ''
+  if (pet.furLength != undefined) {
+    if (query.length < 5) {
+      query = query + ' WHERE'
+    }
+    query = query + ' pet.furLengthId= ' + pet.furLength.Id
+  }
+  if (pet.color !== undefined) {
+    if (query.length < 5) {
+      query = query + ' WHERE'
+    }
+    query = query + ' AND pet.colorId= ' + pet.color.Id
+  }
+  if (pet.breed !== undefined) {
+    if (query.length < 5) {
+      query = query + ' WHERE'
+    }
+    query = query + ' AND pet.breedId= ' + pet.breed.Id
+  }
+  if (pet.size !== undefined) {
+    if (query.length < 5) {
+      query = query + ' WHERE'
+    }
+    query = query + ' AND pet.sizeId= ' + pet.size.Id
+  }
+  if (pet.hasCollar !== undefined) {
+    if (query.length < 5) {
+      query = query + ' WHERE'
+    }
+    query = query + ' AND pet.hasCollar=  ' + pet.hasCollar
+  }
+  return query
+}
