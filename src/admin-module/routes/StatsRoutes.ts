1b1bd2574a940511daee9aaa0ea774ec3e81c79f
Ivan <ivanelisas@gmail.com>
2021-07-30 11:50:20 -0300

    fixes

--- src/admin-module/routes/StatsRoutes.ts
+++ src/admin-module/routes/StatsRoutes.ts
@@ -7,74 +7,74 @@ import statsService from '../services/StatsService'
 const statsRoutes = Router()
 
 statsRoutes.put('/porcentajeUsuariosActivosSobreInactivos', async (req, res) => {
-  try { 
+  try {
     const filter = req.body.filter
-    const activeUsers = await userService.getUsersByStatus(1,filter)
-    const inactiveUsers = await userService.getUsersByStatus(2,filter)
+    const activeUsers = await userService.getUsersByStatus(1, filter)
+    const inactiveUsers = await userService.getUsersByStatus(2, filter)
     return res.json(statsService.calculoDePorcentajeDeUsuariosActivosSobreInactivos(activeUsers, inactiveUsers))
   } catch (error) {
-    res.send(error.message)
+    res.status(403).send(error.message)
   }
 })
 
 statsRoutes.put('/calculatePostLostBreeds', async (req, res) => {
   try {
-    const filter =req.body.filter
+    const filter = req.body.filter
     return res.json(await statsService.calculatePostLostBreeds(filter))
   } catch (error) {
-    res.send(error.message)
+    res.status(403).send(error.message)
   }
 })
 
 statsRoutes.put('/calculateAlertLostBreeds', async (req, res) => {
   try {
-    const filter =req.body.filter
+    const filter = req.body.filter
     return res.json(await statsService.calculateAlertLostBreeds(filter))
   } catch (error) {
-    res.send(error.message)
+    res.status(403).send(error.message)
   }
 })
 
 statsRoutes.put('/calculatePostStatus', async (req, res) => {
   try {
-    const filter =req.body.filter
+    const filter = req.body.filter
     return res.json(await statsService.calculatePostStatus(filter))
   } catch (error) {
-    res.send(error.message)
+    res.status(403).send(error.message)
   }
 })
 
 statsRoutes.put('/porcentajePostActivosSobreInactivos', async (req, res) => {
   try {
     const filter = req.body.filter
-    const activePost = await postService.getPostsByStatus(1,filter)
-    const inactivePost = await postService.getPostsByStatus(2,filter)
+    const activePost = await postService.getPostsByStatus(1, filter)
+    const inactivePost = await postService.getPostsByStatus(2, filter)
     return res.json(statsService.calculoDePorcentajeDePostsActivosSobreInactivos(activePost, inactivePost))
   } catch (error) {
-    res.send(error.message)
+    res.status(403).send(error.message)
   }
 })
 
 statsRoutes.put('/porcentajeDeRazasPerdidas', async (req, res) => {
   try {
     const filter = req.body.filter
-    const activePost = await postService.getPostsByStatus(1,filter)
-    const inactivePost = await postService.getPostsByStatus(2,filter)
+    const activePost = await postService.getPostsByStatus(1, filter)
+    const inactivePost = await postService.getPostsByStatus(2, filter)
     return res.json(statsService.calculoDePorcentajeDePostsActivosSobreInactivos(activePost, inactivePost))
   } catch (error) {
-    res.send(error.message)
+    res.status(403).send(error.message)
   }
 })
 
 statsRoutes.put('/porcentajeDeAlertasActivasSobreInactivas', async (req, res) => {
   try {
     const filter = req.body.filter
-    console.log("FILTER", filter)
-    const activeAlert = await alertService.getAlertsByStatus(1,filter)
-    const inactiveAlert = await alertService.getAlertsByStatus(2,filter)
+    console.log('FILTER', filter)
+    const activeAlert = await alertService.getAlertsByStatus(1, filter)
+    const inactiveAlert = await alertService.getAlertsByStatus(2, filter)
     return res.json(statsService.calculoDePorcentajeDeAlertasActivasSobreInactivas(activeAlert, inactiveAlert))
   } catch (error) {
-    res.send(error.message)
+    res.status(403).send(error.message)
   }
 })
 
